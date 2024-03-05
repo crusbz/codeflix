599b9b078266f9513dc103ab90cafd371fee779e
cruzbs <joaocruzbs@gmail.com>
2024-03-05 12:10:53 -0300

    feat: add header component in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
+import { Header } from './components/Header';
 
 const inter = Inter({ subsets: [] });
 
@@ -7,7 +8,7 @@ export default function Home() {
   return (
     //app wrapper
     <div className='relative h-screen overflow-hidden bg-gradient-to-b lg:h-[140vh]'>
-      {/* Header */}
+      <Header />
       <main className='relative pb-24 pl-4 lg:pl-16'>
         <div className='flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12'>
           <div className='absolute left-0 top-0 -z-10 flex h-[95vh] w-screen flex-col bg-black'>
