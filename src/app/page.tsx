e55ce93e1c1722f3b1b26070502449abb71c896a
cruzbs <joaocruzbs@gmail.com>
2024-03-05 11:36:22 -0300

    add-buttons-and-banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,15 @@ export default function Home() {
     <div className='relative h-full lg:h-[140vh]'>
       <main className='relative pb-24 pl-4 lg:space-y-24 lg:pl-16'>
         <div className='flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12'>
-          <div className='absolute left-0 top-0 -z-10 flex h-[95vh] w-screen flex-col bg-black'></div>
+          <div className='absolute left-0 top-0 -z-10 flex h-[95vh] w-screen flex-col bg-black'>
+            <Image
+              src='/images/banner5.jpg'
+              alt='The Witcher'
+              fill={true}
+              className='h-[65vh] object-cover lg:h-[95vh]'
+            />
+          </div>
+
           <h1 className='text-2xl font-bold md:text-4xl lg:text-7xl'>
             The Witcher
           </h1>
@@ -19,13 +27,14 @@ export default function Home() {
             place in a world where people ofthe prove more wicked than beasts.
           </p>
         </div>
-        <button className='w-24 rounded-full bg-white px-4 py-2 font-bold text-black md:w-32 lg:w-40'>
-          Play
-        </button>
-
-        {/* <button className='w-24 rounded-full bg-gray-700 px-4 py-2 font-bold text-white md:w-32 lg:w-40'>
-          More Info
-        </button> */}
+        <div className='flex space-x-3'>
+          <button className='itens-center flex cursor-pointer gap-x-2 rounded bg-white px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5 md:text-xl'>
+            Play
+          </button>
+          <button className='itens-center flex cursor-pointer gap-x-2 rounded bg-gray-600 px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5 md:text-xl'>
+            More Info
+          </button>
+        </div>
       </main>
     </div>
   );
